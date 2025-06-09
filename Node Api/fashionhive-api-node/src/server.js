e2ac86184bc226c3563a8051680928ef7060697c
yash palav <149261546+YP-100@users.noreply.github.com>
2025-06-09 18:32:01 +0530

    Update server.js port

--- Node Api/fashionhive-api-node/src/server.js
+++ Node Api/fashionhive-api-node/src/server.js
@@ -1,7 +1,7 @@
 const app = require(".");
 const { connectDb } = require("./config/db");
 
-const PORT = 5454;
+const PORT = process.env.Port || 5454;
 app.listen(PORT, async() => {
     await connectDb();
     console.log(`FashionHive api is running on port ${PORT}`);
